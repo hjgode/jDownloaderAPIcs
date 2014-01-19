7219c0d127072f293cb62a7e74433ce2f415475a
hjgode <hjgode@gmail.com>
2014-01-19 17:05:26 +0100

    dataset relation?

--- JDownLoaderAPI/jdownloader_data.cs
+++ JDownLoaderAPI/jdownloader_data.cs
@@ -19,7 +19,7 @@ namespace JDownLoaderAPI
                 _ds.ReadXml(ms);
                 _dtPackages = _ds.Tables[0];
                 _dtFiles = _ds.Tables[1];
-                _dtPackages.ChildRelations.Add(new DataRelation("package_files", _dtPackages.Columns["package_name"], _dtFiles.Columns["file_package"]));
+                _ds.Relations.Add(new DataRelation("package_files", _dtPackages.Columns["package_name"], _dtFiles.Columns["file_package"]));
 
             }
             catch (Exception)
